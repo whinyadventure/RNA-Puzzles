3303902874b5f4ae382d5cdd31f51b2b1e4ebf86
Lukasz Eckert <luki4824@gmail.com>
2020-01-16 20:54:19 +0100

    Hot fix

--- RNAPuzzles/rnapuzzles/views/puzzles/list.py
+++ RNAPuzzles/rnapuzzles/views/puzzles/list.py
@@ -1,45 +1,25 @@
 from django.db.models import Q
 from django.http import HttpResponse
-
-from django.shortcuts import render
-
-from rnapuzzles.models import PuzzleInfo, Challenge, ChallengeFile
-#from rnapuzzles.views import ContactForm
-
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail, BadHeaderError
-from django.urls import reverse
-from django.contrib import messages
 from guardian.decorators import permission_required
 
 from rnapuzzles.models import PuzzleInfo, Challenge, ChallengeFile, datetime
-from rnapuzzles.views.contact.form import ContactForm
-
-
-@permission_required("rnapuzzles.view_puzzleinfo")
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.urls import reverse
 from django.contrib import messages
 
-from rnapuzzles.models import PuzzleInfo, Challenge, ChallengeFile
+from rnapuzzles.models import PuzzleInfo, Challenge
 from rnapuzzles.views.contact.form import ContactForm
 
-
+@permission_required("rnapuzzles.view_puzzleinfo")
 def list_open(request):
 
     template_name = 'puzzles/list_puzzles.html'
     name = 'Open puzzles'
 
-    # email_form = ContactForm(ask=True, user=request.user, puzzle_id=)
-    context = {'list_name': name, 'data': get_data('current_status', 1)}
-
     n = datetime.datetime.now()
     challenges = Challenge.objects.filter(Q(end_date__gte=n)|Q(start_date__lte=n))
 
-
-    challenges = Challenge.objects.filter(current_status=1)
-
     data = []
 
     for challenge in challenges:
@@ -67,37 +47,6 @@ def list_open(request):
             return redirect(reverse('open-puzzles'))
 
     context = {'list_name': name, 'data': data}
-
-
-    data = []
-
-    for challenge in challenges:
-        puzzle_info = PuzzleInfo.objects.get(id=challenge.puzzle_info_id)
-        files = challenge.challengefile_set.all()
-        email_form = ContactForm(user=request.user, challenge=challenge, list=name)
-
-        data.append([puzzle_info, challenge, files, email_form])
-
-    if request.method == 'POST':
-        email_form = ContactForm(request.POST)
-
-        if email_form.is_valid():
-            from_email = email_form.cleaned_data['from_email']
-            subject = email_form.cleaned_data['subject']
-            message = email_form.cleaned_data['message']
-
-            try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
-                messages.add_message(request, messages.SUCCESS, 'Mail was send.')
-
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-
-            return redirect(reverse('open-puzzles'))
-
-    context = {'list_name': name, 'data': data}
-
-
     return render(request, template_name, context)
 
 @permission_required("rnapuzzles.view_puzzleinfo")
@@ -129,36 +78,8 @@ def list_completed(request):
             try:
                 send_mail(subject, message, from_email, ['admin@example.com'])
                 messages.add_message(request, messages.SUCCESS, 'Mail was send.')
-            except:
-                pass
-    challenges = Challenge.objects.filter(result_published=True)
-
-
-    data = []
-
-    for challenge in challenges:
-        puzzle_info = PuzzleInfo.objects.get(id=challenge.puzzle_info_id)
-        files = challenge.challengefile_set.all()
-        email_form = ContactForm(user=request.user, challenge=challenge, list=name)
-
-        data.append([puzzle_info, challenge, files, email_form])
-
-    if request.method == 'POST':
-        email_form = ContactForm(request.POST)
-
-        if email_form.is_valid():
-            from_email = email_form.cleaned_data['from_email']
-            subject = email_form.cleaned_data['subject']
-            message = email_form.cleaned_data['message']
-
-            try:
-                send_mail(subject, message, from_email, ['admin@example.com'])
-                messages.add_message(request, messages.SUCCESS, 'Mail was send.')
-
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
-
-            return redirect(reverse('open-puzzles'))
 
     context = {'list_name': name, 'data': data}
 
