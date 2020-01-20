d5cbc17faca777503f31597b8b03c3d3a81942c1
Małgorzata Frąckowiak <gosia.f5am@gmail.com>
2020-01-20 21:28:41 +0100

    Change profile detail and profile edit layout.

--- RNAPuzzles/rnapuzzles/views/user/updateForm.py
+++ RNAPuzzles/rnapuzzles/views/user/updateForm.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.utils.translation import ugettext_lazy as _
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 from ...models import CustomUser
 
@@ -12,6 +12,27 @@ class Form(forms.ModelForm):
         self.request = kwargs['initial']['request']
         if self.request.user.role == 1:
             self.fields["institution"].widget = forms.HiddenInput()
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('first_name', css_class='form-group col-md-4 mb-0 offset-md-4'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('last_name', css_class='form-group col-md-4 mb-0 offset-md-4'),
+                css_class='form-row '
+            ),
+            Row(
+                Column('institution', css_class='form-group col-md-4 mb-0 offset-md-4'),
+                css_class='form-row '
+            ),
+            Row(
+                Submit('submit', 'Save', css_class="custom-button"),
+                css_class='form-row flex-d justify-content-center'
+            )
+
+        )
 
     class Meta:
         model = CustomUser
